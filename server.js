f84f896dc7871421e2eac988a81e5d0e933d6a9c
DeveloperNatan <natandesouza403@gmail.com>
2024-10-01 19:46:06 -0300

    cors

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
 
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(cors());
 app.use(express.json());
 
 // Rota para criar um produto
-app.post('/api/produtos', async (req, res) => {
+app.post("/api/produtos", async (req, res) => {
   console.log(req.body); // Adicione esta linha para verificar o corpo da requisição
   const { nome, descricao, preco, categoria } = req.body;
-  
+
   try {
     const produto = await prisma.produto.create({
       data: {
@@ -18,17 +20,16 @@ app.post('/api/produtos', async (req, res) => {
         descricao,
         preco,
         categoria,
-      }
+      },
     });
     res.status(201).json(produto);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao criar produto' });
+    res.status(500).json({ error: "Erro ao criar produto" });
   }
 });
 
-
 // Rota para listar produtos
-app.get('/api/produtos', async (req, res) => {
+app.get("/api/produtos", async (req, res) => {
   const produtos = await prisma.produto.findMany();
   res.json(produtos);
 });
